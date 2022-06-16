9771d62a6ffce247f55ea6fcddca2695fcbb3cbe
MercenaryHapy6 <corazoneduardo@att.net>
2022-06-16 13:09:00 -0500

    need to add weblinks selection

--- Unilogin.py
+++ Unilogin.py
@@ -1,23 +1,35 @@
 
 
 # imports
-from cgi import print_arguments
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 
+# setups
 driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
-
 url = 'https://myeagle.hccs.edu/'
-
 driver.get(url)
 
+# create dictionary to store name and URL
+sel_dict = {
+    'name': [],
+    'url': [],
+}
 
-deflog = 'https://hccsaweb.hccs.edu:8080/psp/csprd/?cmd=login&languageCd=ENG&'
 
+# get a list of all options
 text = driver.find_element_by_xpath(
     '/html/body/section/main/div/div[2]/a/div').text
 print(text)
+sel_dict["name"].append(text)
+print(sel_dict)
+
+print(driver.find_element_by_class_name('block-content'))
+print(driver.find_elements_by_class_name('block-content'))
+
+
+defl = 'https://hccsaweb.hccs.edu:8080/psp/csprd/?cmd=login&languageCd=ENG&'
+
 
 selection = input('Where do you want to go? (x): ')
 if selection == "x":
