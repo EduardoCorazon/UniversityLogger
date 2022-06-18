98d46543b55417de56538edf585fa50672d9b8fc
MercenaryHapy6 <corazoneduardo@att.net>
2022-06-17 20:03:22 -0500

    run

--- Unilogin.py
+++ Unilogin.py
@@ -13,12 +13,12 @@ driver.get(url)
 
 
 # get a list of all options
-text = driver.find_element_by_xpath(
-    '/html/body/section/main/div/div[2]/a/div').text
+text = driver.find_element(
+    By.XPATH, '/html/body/section/main/div/div[2]/a/div').text
 print(text)
 
 
-china = driver.find_element_by_class_name('block-content')
+china = driver.find_element(By.CLASS_NAME, 'block-content')
 print(china)
 # print(driver.find_elements_by_class_name('block-content'))
 
@@ -29,15 +29,22 @@ defl = 'https://hccsaweb.hccs.edu:8080/psp/csprd/?cmd=login&languageCd=ENG&'
 selection = input('Where do you want to go? (x): ')
 if selection == "x":
 
-    lnks = driver.find_elements_by_tag_name("a")
-    # traverse list
-    for i in lnks:
-        # get_attribute() to get all href
-        print(i.get_attribute('href'))
-    driver.quit()
+    lik = driver.find_elements(By.TAG_NAME, "a")
+    links = []  # list to store all the links found on website
+    titles = []
+    for i in lik:
+        titles.append(i.get_attribute('title'))
+        links.append(i.get_attribute('href'))
+    print("-----------------------------------------\n")
+    print(links)
+    print("--------------------------\n")
+    print(titles)
 
-
+    print("-----")
+    for a, b in zip(titles, links):
+        print(a, b)
 driver.implicitly_wait(3)
+'''
 
 # for loggin in
 driver.find_element_by_xpath(
@@ -45,3 +52,5 @@ driver.find_element_by_xpath(
 driver.find_element_by_xpath(
     '//*[@id="pwd"]').send_keys('testing')
 # driver.find_element_by_xpath('//*[@id="loginbox"]/font/div[6]/input').click()
+
+'''
